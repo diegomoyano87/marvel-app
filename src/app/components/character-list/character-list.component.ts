fec0fc73fa5406bb3eabadff8c7ea9ec583a60d6
Diego Moyano <diegomoyano_19@hotmail.com>
2024-10-23 01:35:59 -0300

    Se configura el componente que listara todos los personajes

--- src/app/components/character-list/character-list.component.ts
+++ src/app/components/character-list/character-list.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-character-list',
+  template: `
+    <ul>
+      <li *ngFor="let character of characters" (click)="select(character)">
+        {{ character.name }}
+        <button (click)="delete(character.id, $event)">Delete</button>
+      </li>
+    </ul>
+  `,
   standalone: true,
-  imports: [],
-  templateUrl: './character-list.component.html',
-  styleUrl: './character-list.component.css'
+  imports: [NgFor]
 })
 export class CharacterListComponent {
+  @Input() characters: any[] = [];
+  @Output() selectCharacter = new EventEmitter<any>();
+  @Output() deleteCharacter = new EventEmitter<number>();
 
+  select(character: any) {
+    this.selectCharacter.emit(character);
+  }
+
+  delete(characterId: number, event: Event) {
+    event.stopPropagation();
+    this.deleteCharacter.emit(characterId);
+  }
 }
