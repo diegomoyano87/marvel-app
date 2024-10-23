25a8ecc59ab9f8e711a59aa8d2aaee7b4b396e5d
Diego Moyano <diegomoyano_19@hotmail.com>
2024-10-23 01:40:16 -0300

    Se configura el componente de formulario permitiendo agregar un nuevo personaje

--- src/app/components/character-form/character-form.component.ts
+++ src/app/components/character-form/character-form.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-character-form',
+  template: `
+    <h2>Agregar Nuevo Personaje</h2>
+    <label>
+      Nombre:
+      <input [(ngModel)]="newCharacter.name" placeholder="name">
+    </label>
+    <button (click)="add()">Agregar</button>
+  `,
   standalone: true,
-  imports: [],
-  templateUrl: './character-form.component.html',
-  styleUrl: './character-form.component.css'
+  imports: [FormsModule]
 })
 export class CharacterFormComponent {
+  @Output() addCharacter = new EventEmitter<any>();
 
+  newCharacter = { id: 0, name: '' };
+
+  add() {
+    this.newCharacter.id = Date.now();
+    this.addCharacter.emit(this.newCharacter);
+    this.newCharacter = { id: 0, name: '' };
+  }
 }
